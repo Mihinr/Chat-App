72fe1ad7102a44532641f8cbdd0cf1760e833d35
Mihin Ratnayake <mihinratnayake19@gmail.com>
2025-05-15 14:44:32 +0530

    Project Completed

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,12 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import {
+  checkAuth,
+  login,
+  logout,
+  signup,
+  updateProfile,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -7,5 +14,8 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
-export default router;
+router.put("/update-profile", protectRoute, updateProfile);
 
+router.get("/check", protectRoute, checkAuth);
+
+export default router;
